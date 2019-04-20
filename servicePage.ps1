6bfdb98e10cf5f3f41070779a57cc16e00fe174c
Kirill Kravtsov <kravtsov.k@gmail.com>
2019-04-19 20:19:24 -0700

    servicepage

---
+++ servicePage.ps1
@@ -0,0 +1,28 @@
+$headers = @"
+<style>
+table {
+    border: 3px solid #000000;
+    background-color: #7B6BEE;
+    width: 100%;
+    text-align: left;
+    border-collapse: collapse;
+  }
+  table td, table th {
+    border: 1px solid #000000;
+    padding: 5px 4px;
+  }
+  table td {
+    font-size: 13px;
+  }
+  table tr:nth-child(even) {
+    background: #9D9CF5;
+  }
+  table th {
+    font-size: 15px;
+    font-weight: bold;
+    color: #000000;
+    text-align: left;
+  }
+</style>
+"@
+Get-Service | Select-Object Status, Name, DisplayName | ConvertTo-Html -Head $headers | Set-Content .\services.htm
